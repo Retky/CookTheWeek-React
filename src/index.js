2ce0e0e986ed7193daeef6501d407551acefd095
Retky <retkyfox@gmail.com>
2023-12-11 15:58:14 -0600

    Mod files organization by folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './pages/DefaultApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/tests/DefaultApp.test.js
+++ src/tests/DefaultApp.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../pages/DefaultApp';
 
 test('renders learn react link', () => {
   render(<App />);
