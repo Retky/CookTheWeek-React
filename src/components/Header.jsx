7d80fa5fe4ebf474cdef1dd780938bee0329941c
Retky <retkyfox@gmail.com>
2023-12-13 00:06:36 -0600

    Add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,47 @@
-const header = () => (
-  <header>
-    -
-  </header>
-);
+import React, { useEffect, useState } from 'react';
+import {
+  Navbar, Nav, Form, FormControl, Button,
+} from 'react-bootstrap';
+import '../styles/Header.css';
 
-export default header;
+function Header() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        document.getElementById('header').style.top = '0';
+      } else {
+        document.getElementById('header').style.top = '-100px';
+      }
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  return (
+    <Navbar bg="light" expand="lg" id="header" className="fixed-top">
+      <Navbar.Brand href="#home">Recipe App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Nav>
+        <Nav className="ml-auto">
+          <Button variant="primary">Login</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
