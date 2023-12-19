77dc06d0a11c5c57d1a182860c0020cede8e66a6
Retky <retkyfox@gmail.com>
2023-12-18 22:29:08 -0600

    Add UserRecipes page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import './styles/App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import UserRecipes from './pages/UserRecipes';
 
 const App = () => (
   <Router>
     <Header />
     <Switch>
       <Route path="/" element={<HomePage />} />
+      <Route path="/recipes" element={<UserRecipes />} />
     </Switch>
   </Router>
 );
---
+++ src/pages/UserRecipes.jsx
@@ -0,0 +1,7 @@
+const UserRecipes = () => (
+  <div className="header-space">
+    <h1>User Recipes</h1>
+  </div>
+);
+
+export default UserRecipes;
