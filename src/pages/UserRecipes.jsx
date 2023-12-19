9dbb7c02eb3484fe46a09719c573a01fdc2e1ce8
Retky <retkyfox@gmail.com>
2023-12-19 11:50:51 -0600

    Mod basic UserRecipes page

--- src/pages/UserRecipes.jsx
+++ src/pages/UserRecipes.jsx
@@ -1,7 +1,30 @@
-const UserRecipes = () => (
-  <div className="header-space">
-    <h1>User Recipes</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRecipes } from '../store/reducers/userReducer';
+
+const UserRecipes = () => {
+  const dispatch = useDispatch();
+  const recipes = useSelector((store) => store.user);
+
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
+
+  return (
+    <div className="header-space">
+      <h1>My Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            {recipe.name}
+            <br />
+            {recipe.description}
+            <br />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default UserRecipes;
