3e9601e5c7ffc2f131c6d6a13b7ae669389146db
Retky <retkyfox@gmail.com>
2023-12-18 21:07:19 -0600

    Add HomePage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import { Container } from 'react-bootstrap';
+
+const HomePage = () => (
+  <Container fluid className="p-3">
+    <h1 className="text-center">Your App Name</h1>
+  </Container>
+);
+
+export default HomePage;
