272aa8641dbbe1017d110ff44ece94b78f97e77f
Retky <retkyfox@gmail.com>
2023-12-18 21:20:18 -0600

    Mod header spacing in HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
 
 const HomePage = () => (
-  <Container fluid className="p-3">
+  <Container fluid className="p-3 header-space">
     <h1 className="text-center">Your App Name</h1>
   </Container>
 );
