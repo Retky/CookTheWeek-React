42616bd89d932940ee62af3bf7eb63d88288d3f0
Retky <joseluis@thefoxsign.com>
2024-01-05 11:58:18 -0600

    Mod UserRecipes component to use RecipeCard component

--- src/pages/UserRecipes.jsx
+++ src/pages/UserRecipes.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRecipes } from '../store/reducers/userReducer';
-import loadImage from '../images/loadImage.webp';
+import RecipeCard from '../components/RecipeCard';
 import '../styles/UserRecipes.css';
 
 const UserRecipes = () => {
@@ -17,21 +17,7 @@ const UserRecipes = () => {
       <h1 className="text-center my-4">My Recipes</h1>
       <div className="row">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="col-md-6 mb-4">
-            <div className="card">
-              <div className="image-container">
-                <img
-                  src={recipe.image_url ? recipe.image_url : loadImage}
-                  className="card-img-top img-fluid"
-                  alt="recipe"
-                />
-              </div>
-              <div className="card-body">
-                <h5 className="card-title">{recipe.name}</h5>
-                <p className="card-text">{recipe.description}</p>
-              </div>
-            </div>
-          </div>
+          <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </div>
     </div>
