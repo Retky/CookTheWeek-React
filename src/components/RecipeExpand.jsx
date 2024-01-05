b02c07658e95698046847e9ba33c59d620a78afb
Retky <joseluis@thefoxsign.com>
2024-01-05 14:04:50 -0600

    Add RecipeExpand component

---
+++ src/components/RecipeExpand.jsx
@@ -0,0 +1,102 @@
+import PropTypes from 'prop-types';
+import loadImage from '../images/loadImage.webp';
+
+const RecipeExpand = ({ recipe }) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <div className="card mb-4">
+          <div className="image-container">
+            <img
+              src={recipe.image_url ? recipe.image_url : loadImage}
+              className="card-img-top img-fluid"
+              alt="recipe"
+            />
+          </div>
+          <div className="card-body">
+            <h1 className="my-4">{recipe.name}</h1>
+            <p className="card-text">{recipe.description}</p>
+            <p>
+              Portions:
+              {recipe.portions}
+            </p>
+            <p>
+              Difficulty:
+              {recipe.difficulty}
+            </p>
+            <p>
+              Preparation Time:
+              {recipe.preparation_time}
+              {' '}
+              hours
+            </p>
+            <p>
+              Cooking Time:
+              {recipe.cooking_time}
+              {' '}
+              hours
+            </p>
+          </div>
+          <div className="card-body">
+            <h5>Ingredients:</h5>
+            <ul>
+              {recipe.ingredients.map((ingredient) => (
+                <li key={ingredient.id}>
+                  {ingredient.name}
+                  :
+                  {ingredient.quantity}
+                  {' '}
+                  {ingredient.unit}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="card-body">
+            <h5>Steps:</h5>
+            <ol>
+              {recipe.steps.map((step) => (
+                <li key={step.id}>{step.description}</li>
+              ))}
+            </ol>
+          </div>
+          <div className="card-body">
+            <h5>
+              Tips:
+            </h5>
+            {recipe.tips}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+RecipeExpand.propTypes = {
+  recipe: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    portions: PropTypes.number.isRequired,
+    difficulty: PropTypes.number.isRequired,
+    preparation_time: PropTypes.number.isRequired,
+    cooking_time: PropTypes.number.isRequired,
+    tips: PropTypes.string.isRequired,
+    image_url: PropTypes.string,
+    ingredients: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        quantity: PropTypes.number.isRequired,
+        unit: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    steps: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  }).isRequired,
+};
+
+export default RecipeExpand;
